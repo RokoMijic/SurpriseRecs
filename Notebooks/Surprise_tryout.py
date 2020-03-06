53805597a8149e7df3275287a6c5f678468c75a4
r <rmijic@gmail.com>
2020-03-06 16:38:05 +0100

    basic example with Surpr!se

--- Notebooks/Surprise_tryout.py
+++ Notebooks/Surprise_tryout.py
@@ -5,4 +5,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from surprise import SVD
+from surprise import Dataset
+from surprise.model_selection import cross_validate
+
+
+# %%
+# Load the movielens-100k dataset
+data = Dataset.load_builtin('ml-100k')
+
+# %%
+data.raw_ratings
+
+# %%
+algo = SVD()
+
+# %%
+cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+
 # %%
